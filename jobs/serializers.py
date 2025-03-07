b099155565184edba4ff5d2c608954aeef2098e2
stuart <s.scott@casema.nl>
2025-03-07 10:42:21 +0100

    Add customer field to JobsSerializer for improved job-customer association

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import Job
+from customers.models import Customer
 
 class JobsSerializer(serializers.ModelSerializer):
+    customer = serializers.PrimaryKeyRelatedField(queryset=Customer.objects.all())
+
     class Meta:
         model = Job
         fields = '__all__'
